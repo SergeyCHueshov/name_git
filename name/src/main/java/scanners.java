bcefcf7e5ce1c8a7e8272f7bb9cdd54e242bf5d5
SergeyCHueshov <121353788+SergeyCHueshov@users.noreply.github.com>
2022-12-24 23:40:24 +0300

    Add files via upload

---
+++ name/src/main/java/scanners.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class scanners {
+
+    public static void main(String[] args) {
+        System.out.println("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("number: " + a);
+
+
+    }
+}
